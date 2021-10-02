eea306d078b6214bddf6cd49399bdcf379c5a94b
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-02 01:39:42 -0500

    unneeded console.logs

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -64,7 +64,6 @@ router.post('/', (req, res) => {
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
-      console.log(err);
       res.status(400).json(err);
     });
 });
@@ -106,7 +105,6 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
