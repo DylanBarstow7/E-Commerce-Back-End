dff505d5cfb3b897f95007a040cb37ad0d01228f
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-01 19:15:45 -0500

    created tag model

--- models/Tag.js
+++ models/Tag.js
@@ -4,9 +4,26 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
+// define columns
+// * `Tag`
 Tag.init(
   {
-    // define columns
+      // * `id`
+    id:{
+      // * Integer.
+      type: DataTypes.INTEGER,
+      // * Doesn't allow null values.
+      allowNull: false,
+      // * Set as primary key.
+      primaryKey: true,
+      // * Uses auto increment.
+      autoIncrement: true,
+    },
+    // * `tag_name
+    tag_name:{
+      // * String.
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
